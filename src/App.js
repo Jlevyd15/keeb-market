165fe59650415ec56407d76ad5976ab44f71b8dc
Jeremy Levy <jlevyd@gmail.com>
2019-04-21 10:38:34 -0700

    update lambda

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super()
+		this.state = { loading: true, msg: undefined }
+	}
+	componentDidMount() {
+		fetch('/.netlify/functions/hello')
+			.then(response => {
+				console.log(response.body)
+				return response.json()
+				// return response ? response.json() : 'nothing'
+			})
+			.then(json => {
+				console.log(json)
+				this.setState({ loading: false, msg: json.msg })
+			})
+		// fetch('https://randomuser.me/api/?results=10')
+		// .then(res => res.json())
+		// .then(json => console.log('results!', json))
+	}
   render() {
-    return (
+    return this.state.loading ? 'Still loading...' : (
       <div className="App">
         <header className="App-header">
+					<h1>{this.state.msg}</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
