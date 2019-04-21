45a15edb5b6cdbec284d626f73fc36c9562c5700
Jeremy Levy <jlevyd@gmail.com>
2019-04-21 10:50:23 -0700

    update lambda

--- lambdas/hello.js
+++ lambdas/hello.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
 	callback(null, {
 		statusCode: 200,
 		headers: { 'Content-Type': 'application/json' },
-		body: { data: "Hello Netlify" }
+		body: "Hello Netlify"
 	});
 }
--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 	}
 	componentDidMount() {
 		fetch('/.netlify/functions/hello')
+		// fetch('http://localhost:34567/hello')
 			.then(response => {
 				console.log(response.body)
 				return response.json()
